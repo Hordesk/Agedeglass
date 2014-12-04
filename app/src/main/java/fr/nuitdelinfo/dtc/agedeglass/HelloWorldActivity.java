0d17c716b3975e77996b8e1a5841f0859cd3c89f
hordesk <mathias.faure.31@gmail.com>
2014-12-04 21:49:45 +0100

    First commit

---
+++ app/src/main/java/fr/nuitdelinfo/dtc/agedeglass/HelloWorldActivity.java
@@ -0,0 +1,48 @@
+package fr.nuitdelinfo.dtc.agedeglass;
+
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class HelloWorldActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_world);
+
+        /*
+         * Mise en place de police perso sur le txt
+         */
+        TextView helloWorld = (TextView) findViewById(R.id.txt);
+        helloWorld.setTypeface(Typeface.createFromAsset(getAssets(), "Roboto-Thin.ttf"));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_hello_world, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
